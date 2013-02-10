145b1e6a3b178416d860a579c4bf087361e648b5
Niko Ehrenfeuchter <mail@he1ix.org>
2013-02-10 15:38:20 +0100

    working prototype of cgi script

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-sispym
+SisPyM
 ======
 
 Python based web interface for Gembird SIS-PM controllable power outlets.
+
+Requirements
+============
+
+Requires the sispm Python module available from the `extra` section of the sispmctl package (e.g. available from http://github.com/anlumo/sispmctl.git).
---
+++ power.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import cgi
+import cgitb
+cgitb.enable()
+import sispm
+
+def process_sispm():
+    outlet = None
+    action = None
+    fs = cgi.FieldStorage()
+    if 'outlet' in fs:
+        outlet = fs['outlet'].value
+    if 'action' in fs:
+        action = fs['action'].value
+
+    if not ((outlet is None) or (action is None)):
+        status = True
+        if (action == 'True'):
+            status = True
+        if (action == 'False'):
+            status = False
+        dev.set_outlet_enabled(int(outlet), status)
+
+    for outlet in range(dev.get_num_outlets()):
+        status = dev.get_outlet_enabled(outlet)
+        status_name = 'OFF';
+        if status:
+            status_name = 'ON'
+        toggle = "<a href='power.py?outlet=%s&action=%s'>%s</a>" % \
+            (outlet, not(status), status_name)
+        print "Status of outlet %s: %s<br/><br/>" % (outlet, toggle)
+
+
+print "Content-Type: text/html"
+print
+print '<div align="center">'
+print "<a href='power.py'>sispm power control</a><br/><br/>"
+
+try:
+    dev=sispm.Sispm()
+    process_sispm()
+except sispm.SispmException as e:
+    print "error: %s" % e
+
+
+print '</html>'
+
+
+# from subprocess import call
+# from subprocess import Popen, PIPE
+# for socket in [1, 2, 3, 4]:
+#     p = Popen(["sispmctl", "-q", "-g", str(socket)], stdout=PIPE)
+#     out, err = p.communicate()
+#     toggle = "<a href='power_toggle_%s.py'>%s</a>" % (socket, out)
+#     print "Status of outlet %s: %s<br/><br/>" % (socket, toggle)
